a2999f15644b96ebb13740c8c4b70bb88763be3d
Mats Rauhala <mats.rauhala@gmail.com>
2011-03-31 11:10:51 +0300

    Slugify and tests

--- noteit.hs
+++ noteit.hs
@@ -4,7 +4,18 @@ module Main where
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.IO as TI
-import Data.Char (isSpace)
+import Data.Char
+import System.Console.CmdArgs
+import Test.QuickCheck
+
+instance Arbitrary Text where
+  arbitrary = fmap T.pack arbitrary
 
 slug :: Text -> Text
-slug x = T.toLower $ T.map (\y -> if isSpace y then '_' else y) x
+slug x = T.filter (isPrint) $ T.toLower $ T.map (\y -> if isSpace y then '_' else y) x
+
+prop_slug_no_spaces x = property $ T.length (T.dropWhile (not . isSpace) $ slug x) == 0
+prop_slug_all_lower x = property $ T.length (T.filter (isUpper) $ slug x) == 0
+prop_slug_all_printable x = property $ T.length (T.filter (not . isPrint) (slug x)) == 0
+
+main = undefined
